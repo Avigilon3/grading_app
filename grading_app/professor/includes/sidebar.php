1ffb8006c5a95aa525d11682c874d00c42caef8d
Bethel Albor <bethelalbors@gmail.com>
2025-11-22 23:58:59 +0800

    update prof sidebar
    removed settings from the sidebar

--- grading_app/professor/includes/sidebar.php
+++ grading_app/professor/includes/sidebar.php
@@ -5,6 +5,5 @@
     <a href="./my_sections.php" class="nav-item"><img src="../../admin/assets/images/database2.svg" alt="">My Sections</a>
     <a href="./grading_sheet.php" class="nav-item"><img src="../../admin/assets/images/gradingsheets2.svg" alt="">Grading Sheets</a>
     <a href="./requests.php" class="nav-item"><img src="../../admin/assets/images/requests2.svg" alt="">Requests</a>
-    <a href="./settings.php" class="nav-item"><img src="../../admin/assets/images/settings.svg" alt="">Settings</a>
   </nav>
 </aside>
